f48742db3cc1b597c5428a1a0b33b0323c29a3f4
Etienne Caquot <etienne.caquot@epfl.ch>
2017-02-28 22:32:29 +0100

    rien d'interessant, entete des methodes de ContinuoudElevationModel

--- src/ch/epfl/alpano/dem/ContinuousElevationModel.java
+++ src/ch/epfl/alpano/dem/ContinuousElevationModel.java
@@ -1,21 +1,29 @@
 package ch.epfl.alpano.dem;
 
-
 import static java.util.Objects.requireNonNull;
+
+import ch.epfl.alpano.GeoPoint;
 
 /**
  * @author : Jeremy Zerbib (257715)
  */
-public class ContinuousElevationModel {
+public final class ContinuousElevationModel {
 
     private DiscreteElevationModel dem;
 
-    public ContinuousElevationModel(DiscreteElevationModel dem){
+    public ContinuousElevationModel(DiscreteElevationModel dem) {
         this.dem = requireNonNull(dem);
     }
 
-
-    public DiscreteElevationModel dem(){
+    public DiscreteElevationModel dem() {
         return dem;
+    }
+
+    public double elevationAt(GeoPoint p) {
+        return 0;
+    }
+    
+    public double slopeAt(GeoPoint p){
+        return 0;
     }
 }
