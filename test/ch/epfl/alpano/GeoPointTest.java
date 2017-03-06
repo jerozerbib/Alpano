038eaedfdf024e8992b19b9bdc698dd45b1ced52
Etienne Caquot <etienne.caquot@epfl.ch>
2017-03-06 18:11:28 +0100

    Revert "Etienne"

--- test/ch/epfl/alpano/GeoPointTest.java
+++ test/ch/epfl/alpano/GeoPointTest.java
@@ -1,31 +1,56 @@
 package ch.epfl.alpano;
 
-import static java.lang.Math.toDegrees;
-import static java.lang.Math.toRadians;
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author : Jeremy Zerbib (257715)
+ */
 public class GeoPointTest {
-    private static GeoPoint CORNAVIN = new GeoPoint(toRadians(6.14308), toRadians(46.21023));
-    private static GeoPoint M1_EPFL = new GeoPoint(toRadians(6.56599), toRadians(46.52224));
-    private static GeoPoint FEDERAL_PALACE = new GeoPoint(toRadians(7.44428), toRadians(46.94652));
-    private static GeoPoint SAENTIS = new GeoPoint(toRadians(9.34324), toRadians(47.24942));
-    private static GeoPoint MONTE_TAMARO = new GeoPoint(toRadians(8.86598), toRadians(46.10386));
 
-    @Test
-    public void distanceToWorksOnKnownPoints() {
-        assertEquals(226_000, M1_EPFL.distanceTo(SAENTIS), 10);
-        assertEquals( 81_890, M1_EPFL.distanceTo(FEDERAL_PALACE), 10);
-        assertEquals(143_560, FEDERAL_PALACE.distanceTo(MONTE_TAMARO), 10);
-        assertEquals(269_870, SAENTIS.distanceTo(CORNAVIN), 10);
+    @Test(expected = IllegalArgumentException.class)
+    public void isLongitudeThrowingException(){
+        new GeoPoint(-12, 1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void isLatitudeThrowingException(){
+        new GeoPoint(1, -12);
     }
 
     @Test
-    public void azimuthToWorksOnKnownPoints() {
-        assertEquals( 68.03, toDegrees(M1_EPFL.azimuthTo(SAENTIS)), 0.01);
-        assertEquals( 54.50, toDegrees(M1_EPFL.azimuthTo(FEDERAL_PALACE)), 0.01);
-        assertEquals(130.23, toDegrees(FEDERAL_PALACE.azimuthTo(MONTE_TAMARO)), 0.01);
-        assertEquals(245.82, toDegrees(SAENTIS.azimuthTo(CORNAVIN)), 0.01);
+    public void longitudeIsOk(){
+        GeoPoint test = new GeoPoint(1, 0);
+        assertEquals(1, test.longitude(), 0);
     }
+
+    @Test
+    public void latitudeIsOk(){
+        GeoPoint test = new GeoPoint(1, 0);
+        assertEquals(0, test.latitude(), 0);
+    }
+
+    @Test
+    public void distanceTo() throws Exception {
+        GeoPoint c1 = new GeoPoint(Math.toRadians(8.01700), Math.toRadians(46.57306));
+        GeoPoint c2 = new GeoPoint(Math.toRadians(8.017000), Math.toRadians(47.566044));
+        assertEquals(110.4e3, c1.distanceTo(c2), 1e3);
+    }
+
+
+    @Test
+    public void azimuthTo() throws Exception {
+        GeoPoint c1 = new GeoPoint(Math.toRadians(8.01700), Math.toRadians(46.57306));
+        GeoPoint c2 = new GeoPoint(Math.toRadians(8.017000), Math.toRadians(47.566044));
+        assertEquals(0, c1.azimuthTo(c2), 0);
+    }
+
+    @Test
+    public void toStringWorks(){
+        GeoPoint c1 = new GeoPoint(Math.toRadians(-7.6543), Math.toRadians(54.3210));
+        assertEquals("(-7.6543,54.3210)", c1.toString());
+    }
+
+
 }
