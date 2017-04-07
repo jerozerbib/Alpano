d9f67aa58b5a898927d044ea58bc853de7cc065f
Etienne Caquot <etienne.caquot@epfl.ch>
2017-04-07 12:42:40 +0200

    rendu intermédiaire final

--- src/ch/epfl/alpano/PanoramaComputer.java
+++ src/ch/epfl/alpano/PanoramaComputer.java
@@ -22,20 +22,23 @@ public final class PanoramaComputer {
     private final int epsilon = 4;
     private static final double k = 0.13;
 
-
     /**
      * Panorama's Constructor
+     * 
      * @param dem
+     *            the dem to set
      */
     public PanoramaComputer(ContinuousElevationModel dem) {
         this.dem = requireNonNull(dem);
     }
 
     /**
-     * Computes the panorama from given parameters by calculating slopes of differents rays from the
-     * obeserver's position.
+     * Computes the panorama from given parameters by calculating slopes of
+     * differents rays from the obeserver's position.
+     * 
      * @param parameters
-     * @return Panorama
+     *            the parameter we use to compute the Panorama
+     * @return Panorama computed with the parameters
      */
     public Panorama computePanorama(PanoramaParameters parameters) {
         final double maxD = parameters.maxDistance();
@@ -44,19 +47,24 @@ public final class PanoramaComputer {
         Panorama.Builder p = new Panorama.Builder(parameters);
         for (int i = 0; i < parameters.width(); i++) {
             double rayX = 0;
-            ElevationProfile e = new ElevationProfile(dem, obsPos,parameters.azimuthForX(i), maxD);
+            ElevationProfile e = new ElevationProfile(dem, obsPos,
+                    parameters.azimuthForX(i), maxD);
             for (int j = parameters.height() - 1; j >= 0; j--) {
                 double raySlope = parameters.altitudeForY(j);
-                DoubleUnaryOperator f = rayToGroundDistance(e, ray0,tan(raySlope));
-                double lowerBoundFirst = firstIntervalContainingRoot(f, rayX,maxD, dX);
+                DoubleUnaryOperator f = rayToGroundDistance(e, ray0,
+                        tan(raySlope));
+                double lowerBoundFirst = firstIntervalContainingRoot(f, rayX,
+                        maxD, dX);
                 double upperBound = lowerBoundFirst + dX;
                 if (lowerBoundFirst != POSITIVE_INFINITY) {
                     rayX = improveRoot(f, lowerBoundFirst, upperBound, epsilon);
                     p.setDistanceAt(i, j, (float) (rayX / cos(raySlope)))
                             .setElevationAt(i, j, (float) e.elevationAt(rayX))
                             .setSlopeAt(i, j, (float) e.slopeAt(rayX))
-                            .setLongitudeAt(i, j, (float) e.positionAt(rayX).longitude())
-                            .setLatitudeAt(i, j, (float) e.positionAt(rayX).latitude());
+                            .setLongitudeAt(i, j,
+                                    (float) e.positionAt(rayX).longitude())
+                            .setLatitudeAt(i, j,
+                                    (float) e.positionAt(rayX).latitude());
                 }
             }
         }
@@ -64,13 +72,20 @@ public final class PanoramaComputer {
     }
 
     /**
-     * This method helps us build a function that calculates the ray's distance to the ground.
+     * This method helps us build a function that calculates the ray's distance
+     * to the ground.
+     * 
      * @param profile
+     *            the profile
      * @param ray0
+     *            the initial elevation of the ray0
      * @param raySlope
-     * @return DoubleUnaryOperator
+     *            the slope of the ray
+     * @return DoubleUnaryOperator a function that calculates the distance
      */
-    public static DoubleUnaryOperator rayToGroundDistance(ElevationProfile profile, double ray0, double raySlope) {
-        return x -> ray0 + x * raySlope - (profile.elevationAt(x) - ((1 - k) / (2 * EARTH_RADIUS)) * sq(x));
+    public static DoubleUnaryOperator rayToGroundDistance(
+            ElevationProfile profile, double ray0, double raySlope) {
+        return x -> ray0 + x * raySlope - (profile.elevationAt(x)
+                - ((1 - k) / (2 * EARTH_RADIUS)) * sq(x));
     }
 }
