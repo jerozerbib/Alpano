a76b9f339fb25f0a88887b9c9aad79286a89a3ea
Etienne Caquot <etienne.caquot@epfl.ch>
2017-05-21 12:40:26 +0200

    Commentaire et fix étape 8

--- src/ch/epfl/alpano/gui/PredefinedPanoramas.java
+++ src/ch/epfl/alpano/gui/PredefinedPanoramas.java
@@ -2,14 +2,44 @@ package ch.epfl.alpano.gui;
 
 /**
  * @author : Jeremy Zerbib (257715)
+ * @author : Etienne Caquot (249949)
  */
-public class PredefinedPanoramas {
 
-    //TODO : Magic NUm
-    static PanoramaUserParameters NIESEN = new PanoramaUserParameters(76_500, 467_300, 600, 180, 110, 300, 2500, 800, 0);
-    static PanoramaUserParameters JURA = new PanoramaUserParameters(68_087, 470_085, 1_380, 162, 27, 300, 2500, 800, 0);
-    static PanoramaUserParameters RACINE = new PanoramaUserParameters(68_200, 470_200, 1_500, 135, 45, 300, 2500, 800, 0);
-    static PanoramaUserParameters FINSTERAAHORN = new PanoramaUserParameters(81_260, 465_374, 4_300, 205, 20, 300, 2500, 800, 0);
-    static PanoramaUserParameters SAUVABELIN = new PanoramaUserParameters(66_385, 465_353, 700, 135, 100, 300, 2500, 800, 0);
-    static PanoramaUserParameters PELICAN = new PanoramaUserParameters(65_728, 465_132, 380, 135, 60, 300, 2500, 800, 0);
+public interface PredefinedPanoramas {
+
+    /**
+     * Predefined UserParameters for NIESEN
+     */
+    static PanoramaUserParameters NIESEN = new PanoramaUserParameters(76_500,
+            467_300, 600, 180, 110, 300, 2_500, 800, 0);
+
+    /**
+     * Predefined UserParameters for JURA
+     */
+    static PanoramaUserParameters JURA = new PanoramaUserParameters(68_087,
+            470_085, 1_380, 162, 27, 300, 2_500, 800, 0);
+
+    /**
+     * Predefined UserParameters for RACINE
+     */
+    static PanoramaUserParameters RACINE = new PanoramaUserParameters(68_200,
+            470_200, 1_500, 135, 45, 300, 2_500, 800, 0);
+
+    /**
+     * Predefined UserParameters for FINSTERAAHORN
+     */
+    static PanoramaUserParameters FINSTERAAHORN = new PanoramaUserParameters(
+            81_260, 465_374, 4_300, 205, 20, 300, 2_500, 800, 0);
+
+    /**
+     * Predefined UserParameters for SAUVABELIN
+     */
+    static PanoramaUserParameters SAUVABELIN = new PanoramaUserParameters(
+            66_385, 465_353, 700, 135, 100, 300, 2_500, 800, 0);
+
+    /**
+     * Predefined UserParameters for PELICAN
+     */
+    static PanoramaUserParameters PELICAN = new PanoramaUserParameters(65_728,
+            465_132, 380, 135, 60, 300, 2_500, 800, 0);
 }
--- src/ch/epfl/alpano/gui/UserParameter.java
+++ src/ch/epfl/alpano/gui/UserParameter.java
@@ -5,26 +5,39 @@ import static java.lang.Math.min;
 
 /**
  * @author : Jeremy Zerbib (257715)
+ * @author : Etienne Caquot (249949)
  */
-public enum  UserParameter {
-    OBSERVER_LONGITUDE(60_000, 120_000),
-    OBSERVER_LATITUDE(450_000, 480_000),
-    OBSERVER_ELEVATION(300, 10_000),
-    CENTER_AZIMUTH(0, 359),
-    HORIZONTAL_FIELD_OF_VIEW(1, 360),
-    MAX_DISTANCE(10, 600),
-    WIDTH(30, 16_000),
-    HEIGHT(100, 4_000),
-    SUPER_SAMPLING_EXPONENT(0, 2);
+
+public enum UserParameter {
+    OBSERVER_LONGITUDE(60_000, 120_000), OBSERVER_LATITUDE(450_000,
+            480_000), OBSERVER_ELEVATION(300, 10_000), CENTER_AZIMUTH(0,
+                    359), HORIZONTAL_FIELD_OF_VIEW(1, 360), MAX_DISTANCE(10,
+                            600), WIDTH(30, 16_000), HEIGHT(100,
+                                    4_000), SUPER_SAMPLING_EXPONENT(0, 2);
 
     private final int min;
     private final int max;
 
-    UserParameter(int min, int max){
+    /**
+     * UserParameter's constructor
+     * 
+     * @param min
+     *            the minimum value to set
+     * @param max
+     *            the maximum value to set
+     */
+    private UserParameter(int min, int max) {
         this.min = min;
         this.max = max;
     }
 
+    /**
+     * Returns the closest valid value.
+     * 
+     * @param v
+     *            the value to check
+     * @return the corresponding closest valid value
+     */
     public int sanitize(int v) {
         return max(min, min(v, max));
     }
